2b2ae4a1100da8ee7d4feea6df903e6cf22e423c
Hoang20040211 <hoang110204gmail.com>
2024-03-11 12:47:42 +0700

    duan1

---
+++ app/views/Client/layout/header.php
@@ -0,0 +1,74 @@
+
+
+
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+        <link rel="stylesheet" href="public/css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <header>
+            <div class="boxcenter">
+                <div class="logo">
+                    <div class="row">
+                        <div style="" class="col-md-2">
+                            <a href="#">
+                                <img  border="5px" style="border-radius:10px; color:white; margin-left:30px" src="public/img/log.png" alt="" >
+                            </a>
+                        </div>
+                        <div style="" class="col-md-6 ali_center">
+                            <form class="seach-home" action="index.php?redirect=search" method="post">
+                                <div class="input-group rounded">
+                                    <input type="search" class="form-control rounded" placeholder="Search" name="search_input" aria-label="Search" aria-describedby="search-addon" />
+                                    <span class="input-group-text border-0" id="search-addon">
+                                        <button style="border:none" type="submit" name="search"><i class="bi bi-search"></i></button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div style="" class="col-md-4 ali_center">
+                            <ul class="login-header">
+                                <li>
+                                    <a href="index.php?redirect=giohang">
+                                    <span>
+                                        <i class="bi bi-cart"></i>
+                                    </span>
+                                        Giỏ hàng
+                                    </a>
+                                </li>
+                                <div class="login-hd">
+                                <li>
+                                    <a href="index.php?redirect=dangxuat" id="taikhoan">
+                                    <span><i class="bi bi-person-fill"></i></span>
+                                        ĐĂNG XUẤT
+                                    </a>
+                                    <a href="index.php?redirect=doimatkhau" id="taikhoan">
+                                    <span><i class="bi bi-person-fill"></i></span>
+                                        ĐỔI MẬT KHẨU
+                                    </a>
+                                    <a href="app/views/admin/index.php" id="taikhoan">
+                                    <span><i class="bi bi-person-fill"></i></span>
+                                        TRUY CẬP CPANEL
+                                    </a>
+                                                                        
+                                </li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div style="" id="menu">
+                    <ul>
+                        <li style=""><a href="index.php">Trang chủ</a></li>
+                        <li style=""><a href="index.php?redirect=donhang">Đơn hàng </a></li>
+                        <li style=""><a href="index.php?redirect=dmsp">Sản phẩm</a></li>
+                        <li style=""><a href="index.php?redirect=tintuc">Tin tức</a></li>
+                        <li style=""><a href="index.php?redirect=lienhe">Liên hệ</a></li>
+                    </ul>
+                </div>
---
+++ app/views/Client/layout/home.php
@@ -0,0 +1,449 @@
+
+<div class="menu_hanggiay">
+    <div class="row">
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=19">
+                    <img src="public/img/adidas.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=20">
+                    <img src="public/img/nike.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=22">
+                    <img src="public/img/balance.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=23">
+                    <img src="public/img/puma.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=24">
+                    <img src="public/img/converse.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2 ">
+            <div class="item_hanggiay">
+                <a href="index.php?redirect=sptheodanhmuc&id=25">
+                    <img src="public/img/vans.jpg" alt="">
+                </a>
+            </div>
+        </div>
+            
+    </div>
+</div>
+</div>
+</header>
+<main>
+<div class="banner_main">
+<div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="public/img/balancedm.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="public/img/banner3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="public/img/banner2nike.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+</div>
+<div class="contac_1">
+<div class="boxcenter">
+    <div class="title_home">
+        <h2>
+            <a href="index.php?redirect=sptheodanhmuc&id=19">
+                ADIDAS
+            </a>
+        </h2>
+    </div>
+    <div class="item_home">
+        <div class="row">
+            <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas1.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Adidas 1</p>
+                    <p class="gioi_thieu">Giầy thể thao nam 
+</p>
+                    <span>750000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=48">Xem chi tiết</a>
+                </div>
+            </div>
+            <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas2.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Adidas 2</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>240000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=46">Xem chi tiết</a>
+                </div>
+            </div>
+            <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas3.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Adidas 3</p>
+                    <p class="gioi_thieu">Giầy thể thao nam </p>
+                    <span>600000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=45">Xem chi tiết</a>
+                </div>
+            </div>
+            <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas4.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Adidas 4</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>400000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=44">Xem chi tiết</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="xem_them">
+        <a href="#">
+            xem thêm sản phẩm
+        </a>
+    </div>
+</div>
+</div>
+<div class="contac_1">
+<div class="boxcenter">
+    <div class="title_home">
+        <h2>
+            <a href="index.php?redirect=sptheodanhmuc&id=20">
+                NIKE
+            </a>
+        </h2>
+    </div>
+    <div class="item_home">
+        <div class="row">
+            <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/sp4 copy.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Nike 1</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>150000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=39">Xem chi tiết</a>
+                </div>
+            </div>
+                <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/sp2 copy.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Nike 2</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>750000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=38">Xem chi tiết</a>
+                </div>
+            </div>
+                <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/sp3 copy.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Nike 3</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>400000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=37">Xem chi tiết</a>
+                </div>
+            </div>
+                <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/sp4 copy 2.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Nike 4</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>100000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=36">Xem chi tiết</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="xem_them">
+        <a href="#">
+            xem thêm sản phẩm
+        </a>
+    </div>
+</div>
+</div>
+<div class="contac_1">
+<div class="boxcenter">
+    <div class="title_home">
+        <h2>
+            <a href="index.php?redirect=sptheodanhmuc&id=23">
+            Christian dior
+            </a>
+        </h2>
+    </div>
+    <div class="item_home">
+        <div class="row">
+                                    <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/balance.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Puma 4</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>600000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=49">Xem chi tiết</a>
+                </div>
+            </div>
+                                    <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas1.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Puma 1</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>150000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=35">Xem chi tiết</a>
+                </div>
+            </div>
+                                    <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/converse.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Puma 2</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>400000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=34">Xem chi tiết</a>
+                </div>
+            </div>
+                                    <div class="col-md-3 box-item">
+                <div class="img_item zoom_img">
+                    <div>
+                        <img src="public/img/adidas1.png" alt="">
+                    </div>
+                </div>
+                <div class="text_item">
+                    <p class="loai_hang">Giầy chạy bộ Puma 3</p>
+                    <p class="gioi_thieu">Giầy thể thao nam</p>
+                    <span>150000</span>
+                </div>
+                <div class="item_contac">
+                    <a href="index.php?redirect=ctsp&id=33">Xem chi tiết</a>
+                </div>
+            </div>
+                                    </div>
+        </div>
+    </div>
+    <div class="xem_them">
+        <a href="#">
+            xem thêm sản phẩm
+        </a>
+    </div>
+</div>
+</div>
+<div class="contac_3">
+<div class="boxcenter">
+    <div class="title_home">
+        <h2>
+            <a href="#">
+                Danh mục mới
+            </a>
+        </h2>
+    </div>
+    
+    <div class="row">
+                        <div class="col-md-4">
+            <div class="box-item">
+                <div class="img_box">
+                    <img src="public/img/asics.jpg" alt="">
+                </div>
+                <div class="text_box">
+                    <h3>
+                        Asic                                </h3>
+                    <a href="index.php?redirect=sptheodanhmuc&id=26">xem thêm</a>
+                </div>
+            </div>
+        </div>
+                        <div class="col-md-4">
+            <div class="box-item">
+                <div class="img_box">
+                    <img src="public/img/vans.jpg" alt="">
+                </div>
+                <div class="text_box">
+                    <h3>
+                        Vans                                </h3>
+                    <a href="index.php?redirect=sptheodanhmuc&id=25">xem thêm</a>
+                </div>
+            </div>
+        </div>
+                        <div class="col-md-4">
+            <div class="box-item">
+                <div class="img_box">
+                    <img src="public/img/converse.jpg" alt="">
+                </div>
+                <div class="text_box">
+                    <h3>
+                        converse                                </h3>
+                    <a href="index.php?redirect=sptheodanhmuc&id=24">xem thêm</a>
+                </div>
+            </div>
+        </div>
+                    </div>
+</div>
+</div>
+<div class="contac_2">
+<div class="title_home">
+    <h2>
+        <a href="#">
+            tin tức
+        </a>
+    </h2>
+</div>
+<div class="boxcenter">
+    <div class="news_item">
+        <div class="row">
+            <div class="col-md-3">
+                <a href="#" class="item_news">
+                    <div class="img_news">
+                        <img src="public/img/vansbn.jpg" alt="">
+                    </div>
+                    <div class="text_news">
+                        <h3>SẢN XUẤT TẠI VIỆT NAM
+                        </h3>
+                        <p>
+                        Hot family
+Gia đình Chuyện nhà Đậu rất tin dùng các sản phẩm công nghệ chính hãng mua tại DHA SHOP. Phải công nhận rằng cũng nhờ vào những thiết bị này mà chúng mình mới có thể quay thêm nhiều vlog thú vị cho khán giả xem. Hãy đến các chi nhánh của DHASHOP trên toàn quốc để mua sắm và trải nghiệm nhé!
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="item_news">
+                    <div class="img_news">
+                        <img src="public/img/dior.jpg" alt="">
+                    </div>
+                    <div class="text_news">
+                        <h3>SẢN XUẤT TẠI VIỆT NAM
+                        </h3>
+                        <p>
+                        Hot family
+Gia đình Chuyện nhà Đậu rất tin dùng các sản phẩm chính hãng mua tại DHA SHOP. Phải công nhận rằng cũng nhờ vào những thiết bị này mà chúng mình mới có thể quay thêm nhiều vlog thú vị cho khán giả xem. Hãy đến các chi nhánh của DHASHOP trên toàn quốc để mua sắm và trải nghiệm nhé!
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="item_news">
+                    <div class="img_news">
+                        <img src="public/img/gc.jpg" alt="">
+                    </div>
+                    <div class="text_news">
+                        <h3>SẢN XUẤT TẠI VIỆT NAM
+                        </h3>
+                        <p>
+                        Hot family
+Gia đình Chuyện nhà Đậu rất tin dùng các sản phẩm chính hãng mua tại DHA SHOP. Phải công nhận rằng cũng nhờ vào những thiết bị này mà chúng mình mới có thể quay thêm nhiều vlog thú vị cho khán giả xem. Hãy đến các chi nhánh của DHASHOP trên toàn quốc để mua sắm và trải nghiệm nhé!
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="item_news">
+                    <div class="img_news">
+                        <img src="public/img/nikedm.png" alt="">
+                    </div>
+                    <div class="text_news">
+                        <h3>SẢN XUẤT TẠI VIỆT NAM
+                        </h3>
+                        <p>
+                        Hot family
+Gia đình Chuyện nhà Đậu rất tin dùng các sản phẩm chính hãng mua tại DHA SHOP. Phải công nhận rằng cũng nhờ vào những thiết bị này mà chúng mình mới có thể quay thêm nhiều vlog thú vị cho khán giả xem. Hãy đến các chi nhánh của DTCMobile trên toàn quốc để mua sắm và trải nghiệm nhé!
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</main>
