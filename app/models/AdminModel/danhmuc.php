b4bc85a06690b6920f1f3c23ce203a89e145f8ec
Hoang20040211 <hoang110204@gmail.com>
2024-03-19 20:35:44 +0700

    dm

--- app/models/AdminModel/danhmuc.php
+++ app/models/AdminModel/danhmuc.php
@@ -1,4 +1,10 @@
 <?php
+
+function insert_danhmuc($tenloai,$hinh){
+    $sql = "insert into danhmuc(name,img) values('$tenloai','$hinh')";
+    pdo_execute($sql);
+}
+
 function delete_danhmuc($id){
     $sql = "delete from danhmuc where id=".$id;
     pdo_execute($sql);
@@ -16,4 +22,11 @@ function loadone_danhmuc($id){
     return $dm;
 }
 
-
+function update_danhmuc($id,$tenloai,$hinh){
+    if($hinh != ""){
+        $sql = "UPDATE danhmuc set name= '$tenloai' , img = '$hinh' where id= $id";
+    }else{
+        $sql = "UPDATE danhmuc set name= '$tenloai' where id= $id";
+    }
+    pdo_execute($sql);
+}
