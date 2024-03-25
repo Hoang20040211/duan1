5e84a6c4e57c10a493f5ced5103a1806c7c29e2d
dung262004 <dungnkph44873@fpt.edu.vn>
2024-03-25 12:25:28 +0700

    dung

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "app/models/clientModel/sanpham.php";
 include "app/models/clientModel/danhmuc.php";
 // include "app/models/clientModel/binhluan.php";
 include "app/models/clientModel/taikhoan.php";
-
+/** */
 include "config.php";
 include "app/views/Client/layout/header.php";
 // include "app/views/Client/layout/home.php";
